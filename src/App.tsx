57e80e896b20dd0ee3bb25b1c9ae0bca2d4751e4
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-03-23 14:58:01 +0100

    added music

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState, useRef } from "react";
+import "./navbar.css";
+
+const music = [
+  "music/music-1.mp3",
+  "music/music-2.mp3",
+  "music/music-3.mp3",
+  "music/music-4.mp3",
+];
+
+function Navbar() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(music[1]);
+  const audioElem = useRef<HTMLAudioElement | null>(null);
+
+  const changeVolume = () => {
+    if (audioElem.current) {
+      audioElem.current.volume = 0.1;
+    }
+  };
+  changeVolume();
+
+  const next = () => {
+    const index = music.findIndex((item) => item === currentSong);
+    if (index === music.length - 1) {
+      setCurrentSong(music[0]);
+    } else {
+      setCurrentSong(music[index + 1]);
+    }
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElem.current?.play();
+    } else {
+      audioElem.current?.pause();
+    }
+  }, [isPlaying, currentSong]);
+
+  return (
+    <header className="navbar-container">
+      <div className="music-container">
+        <audio ref={audioElem} src={currentSong}></audio>
+        <img
+          className="music-icon"
+          src="images/icons/music.png"
+          alt="play music icon"
+          onClick={() => setIsPlaying(false)}
+        />
+        {!isPlaying && (
+          <img
+            className="cross-icon"
+            src="images/icons/cross.png"
+            alt="stopped to play music icon"
+            onClick={() => setIsPlaying(true)}
+          />
+        )}
+        <img
+          className="next-icon"
+          src="images/icons/next-arrow.png"
+          alt="next music icon"
+          onClick={next}
+        />
+      </div>
+
+      <div className="user-rating-container">
+        <div className="user-container">
+          <img
+            className="user-icon"
+            src="images/icons/user.png"
+            alt="user icon"
+          />
+          <p className="user-name">User name</p>
+          <img
+            className="pencil-icon"
+            src="images/icons/pencil.png"
+            alt="edit user name"
+          />
+        </div>
+        <img
+          className="cup-icon"
+          src="images/icons/cup.png"
+          alt="open and close rating icon"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
